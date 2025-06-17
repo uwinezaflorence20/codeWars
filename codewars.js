6e6f874f18323c3924b3537da0de3d235927a4a9
Uwineza Florence <uwinezaflorence20@gmail.com>
2025-06-17 14:49:35 +0200

    done

--- codewars.js
+++ codewars.js
@@ -13,3 +13,27 @@ function datingRange(age){
 console.log(datingRange(20));
 console.log(datingRange(12));
 console.log(datingRange(40));
+
+function padIt(str,n){
+  //coding here
+//   var cha ="";
+//   var sig="*";
+//   var i =0
+  
+//   while(i<=n){
+//     if(i%2===0){
+//       cha = cha + sig;
+//     }
+//     else{
+//       cha = sig+str;
+//     }
+//     i++; 
+//   }
+//   return cha;
+  
+  var pad = "*";
+  var left = Math.ceil(n/2), right = n-left;
+  return pad.repeat(left) + str+ pad.repeat(right);
+  
+}
+console.log(padIt("a",10));
