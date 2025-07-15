2aaec7628b0de2ab3cf082e32924a4e513e59337
Uwineza Florence <uwinezaflorence20@gmail.com>
2025-07-15 07:47:39 +0200

    done

--- 6kyu.js
+++ 6kyu.js
@@ -12,3 +12,40 @@ function one(arr, fun){
   }
   return count === 1;
 }
+
+function getAges(sum,difference){
+  if (sum < 0 || difference < 0) return null;
+
+  const older = (sum + difference) / 2;
+  const younger = (sum - difference) / 2;
+
+  if (older < 0 || younger < 0) return null;
+    
+  return [older, younger];
+};
+
+function modifyMultiply (str,loc,num) {
+  //Code here
+  const words = str.split(" ");          
+  const word = words[loc];               
+  const repeated = Array(num).fill(word); 
+  return repeated.join("-"); 
+} 
+
+function solve(arr){
+  for (let num of arr) {
+    if (!arr.includes(-num)) {
+      return num;
+    }
+  }
+};
+
+function sortByBinaryOnes(list){
+  // your code here
+  return list.sort((a, b) => {
+    const aBin = a.toString(2), bBin = b.toString(2);
+    const countOnes = s => s.split('').filter(c => c === '1').length;
+    const aOnes = countOnes(aBin), bOnes = countOnes(bBin);
+    return bOnes - aOnes || aBin.length - bBin.length || a - b;
+  });
+}
