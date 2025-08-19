5f121653d686b55c41cd46ea40b04e67a09c68f3
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-08-19 10:57:21 +0200

    remaining one

---
+++ ES6/Udacity.js
@@ -0,0 +1,73 @@
+/*
+ * Programming Quiz: Using Let and Const (1-1)
+ */
+
+const CHARACTER_LIMIT = 255;
+const posts = [
+	"#DeepLearning transforms everything from self-driving cars to language translations. AND it's our new Nanodegree!",
+	"Within your first week of the VR Developer Nanodegree Program, you'll make your own virtual reality app",
+	"I just finished @udacity's Front-End Web Developer Nanodegree. Check it out!"
+];
+
+// prints posts to the console
+function displayPosts() {
+	for (let i = 0; i < posts.length; i++) {
+		console.log(posts[i].slice(0, CHARACTER_LIMIT));
+	}
+}
+
+displayPosts();
+
+
+function getClothing(isCold) {
+  if (isCold) {
+    let freezing = 'Grab a jacket!'; 
+  } else {
+    let hot = 'It’s a shorts kind of day.'; 
+    console.log(freezing);
+  }
+}
+
+getClothing(false);
+
+const myName = 'Florence';
+const greeting = `Hello, my name is ${myName}`;
+console.log(greeting);
+
+/*
+ * Programming Quiz: Build an HTML Fragment (1-2)
+ */
+
+const cheetah = {
+    name: 'Cheetah',
+    scientificName: 'Acinonyx jubatus',
+    lifespan: '10-12 years',
+    speed: '68-75 mph',
+    diet: 'carnivore',
+    summary: 'Fastest mammal on land, the cheetah can reach speeds of 60 or perhaps even 70 miles (97 or 113 kilometers) an hour over short distances. It usually chases its prey at only about half that speed, however. After a chase, a cheetah needs half an hour to catch its breath before it can eat.',
+    fact: 'Cheetahs have “tear marks” that run from the inside corners of their eyes down to the outside edges of their mouth.'
+};
+
+// creates an animal trading card
+function createAnimalTradingCardHTML(animal) {
+    const cardHTML = `
+    <div class="card">
+        <h3 class="name">${animal.name}</h3>
+        <img src="${animal.name}.jpg" alt="${animal.name}" class="picture">
+        <div class="description">
+            <p class="fact">${animal.fact}</p>
+            <ul class="details">
+                <li><span class="bold">Scientific Name</span>: ${animal.scientificName}</li>
+                <li><span class="bold">Average Lifespan</span>: ${animal.lifespan}</li>
+                <li><span class="bold">Average Speed</span>: ${animal.speed}</li>
+                <li><span class="bold">Diet</span>: ${animal.diet}</li>
+            </ul>
+            <p class="brief">${animal.summary}</p>
+        </div>
+    </div>
+    `;
+    return cardHTML;
+}
+
+
+console.log(createAnimalTradingCardHTML(cheetah));
