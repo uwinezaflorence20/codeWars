23cb8c9e2a595b45e648b007a0353d289919de9a
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-09-10 11:02:02 +0200

    done

--- ES6/Symbols in ECMA.js
+++ ES6/Symbols in ECMA.js
@@ -7,6 +7,7 @@ const bowl2 = {
   'banana': { color: 'yellow', weight: 183.15 },
   'orange': { color: 'orange', weight: 170.097 }
 };
+console.log(bowl2)
 console.log(bowl2.apple.color);
 console.log(bowl2.apple);
 
@@ -16,7 +17,7 @@ const bowl = {
   [Symbol('orange')]: { color: 'orange', weight: 170.097 },
   [Symbol('banana')]: { color: 'yellow', weight: 176.845 }
 };
-
+console.log(bowl)
 const symbols = Object.getOwnPropertySymbols(bowl);
 console.log(typeof bowl);
 console.log(bowl[symbols[0]]);
@@ -28,6 +29,51 @@ const bananas = symbols.filter(sym => sym.description === "banana");
 bananas.forEach(bananaSym => {
   console.log(bowl[bananaSym]);
 });
+
+
+const bowl4 = {
+  [Symbol('apple')]: { color1: 'red', weight: 136.078 },
+  [Symbol('banana')]: { color1: 'yellow', weight: 183.15 },
+  [Symbol('orange')]: { color1: 'orange', weight: 170.097 },
+  [Symbol('banana')]: { color1: 'yellow', weight: 176.845 }
+};
+
+let itrator = bowl4[Symbol.iterator]
+for(let [key,value] in itrator){
+  console.log(key,value);
+}
+
+
+const fruits = {
+  apple: { color: "red", weight: 136.078 },
+  banana: { color: "yellow", weight: 183.15 },
+  orange: { color: "orange", weight: 170.097 }
+};
+
+// EASY way: use generator
+fruits[Symbol.iterator] = function* () {
+  for (let key of Object.keys(this)) {
+    yield [key, this[key]];
+  }
+};
+
+// Now you can use for...of
+for (let [key, value] of fruits) {
+  console.log(key, value);
+}
+
+
+const fruits = {
+  apple: { color: "red", weight: 136.078 },
+  banana: { color: "yellow", weight: 183.15 },
+  orange: { color: "orange", weight: 170.097 }
+};
+
+// Using Object.entries()
+for (let [key, value] of Object.entries(fruits)) {
+  console.log(key, value);
+}
+
 
 
 
---
+++ ES6/generator.js
@@ -0,0 +1,16 @@
+function* generate() {
+    console.log('invoked 1st time');
+    yield 1;
+    console.log('invoked 2nd time');
+    yield 2;
+}
+let gen = generate();
+let iter = gen.next();
+let iter2 = gen.next();
+
+console.log(iter)
+console.log(iter2)
+console.log(iter)
+console.log(iter)
+console.log(gen)
+console.log(gen)
