2bf5c1cedc22eb921646aa00375d84b086108621
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-09-25 12:09:57 +0200

    done

--- python.js/function.py
+++ python.js/function.py
@@ -23,3 +23,28 @@ def even_odd(number):
 
 even_odd(10)
 even_odd(7)
+
+
+# set,list
+
+fruits = ["Mango", "Banana", "Apple", "Orange"]
+print(fruits[1])
+
+favorite_book = {
+    "title": "The Alchemist",
+    "author": "Paulo Coelho",
+    "genre": "Fiction"
+}
+print(favorite_book.get("genre"))
+
+import random
+numbers = [random.randint(1, 10) for _ in range(15)]
+unique_numbers = set(numbers)
+print(unique_numbers)
+
+#  math function
+
+import math_function
+
+result = math_functions.add(5, 3)
+print(result)
