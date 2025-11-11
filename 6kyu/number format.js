1ebf376f63d654b377d301ffef4ac15c74b0e722
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-11-11 19:02:20 +0200

    Solve 3 kata today 2 is 7kyu and 1 is 6kyu

---
+++ 6kyu/number format.js
@@ -0,0 +1,3 @@
+function numberFormat(number) {
+  return number.toLocaleString();
+};
---
+++ revising different method/filter.js
@@ -0,0 +1,5 @@
+function numberFormat(number) {
+  let f1 = number.filter(x => x>10);
+  console.log(f1)
+};
+console.log(numberFormat([1,4,2,60,45,3,78]))
