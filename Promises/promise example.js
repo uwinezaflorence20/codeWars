7e68e73fc2692cfbdea88721d629d9516e563984
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-11-14 14:21:27 +0200

    done

--- Promises/promise example.js
+++ Promises/promise example.js
@@ -34,3 +34,14 @@ function added(){
 }
 let promise2 = new Promise(added);
 
+//first example 
+console.log('This is the last line of code in app.js.')
+// Keep the line above as the first line of code
+// Write your code here:
+function usingSTO() {
+  console.log("Hello from usingSTO!");
+}
+setTimeout(usingSTO,3000);
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
+
