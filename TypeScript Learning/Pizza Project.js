96b19140f81cdcd2ed9f444661e4ef192bab733c
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-01-05 18:03:18 +0200

    done with the second push of the year

--- TypeScript Learning/Pizza Project.js
+++ TypeScript Learning/Pizza Project.js
@@ -18,3 +18,24 @@ function addNewPizza(pizza){
     return menu.push(pizza);
 }
 console.log(addNewPizza({name:"Piza",price:8}))
+
+// challenge 2
+function placeOrder(pizzaName){
+    let pizza = menu.find(user => user.name === pizzaName);
+
+        cashInRegister += pizza.price;
+        
+        let newOrder= { pizza:pizza, status: "ordered" }
+        orderQueue.push(newOrder);
+        return newOrder; 
+}
+
+/**
+ * Write another utility function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ */
+
