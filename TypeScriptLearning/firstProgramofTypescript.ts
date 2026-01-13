9b117581c66a86fab13c7f412557cd6b8c1e0d24
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-01-13 14:23:47 +0200

    done

--- TypeScriptLearning/firstProgramofTypescript.ts
+++ TypeScriptLearning/firstProgramofTypescript.ts
@@ -1,6 +1,49 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
+type Pizza ={
+  name : string
+  price: number
+}
+type Order ={
+  id : number
+  pizza:Pizza
+  status: "ordered" | "complete"
+}
+const menu:Pizza[] = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+let cashInRegister = 100
+let nextOrderId = 1
+const orderQueue=[{}]
+
+function addNewPizza(pizzaObj: { name: string; cost?: number; price?: number }) {
+    menu.push( { name: "Margherita", price: 8 })
 }
 
-const message: string = greet("World");
-console.log(message);
+function placeOrder(pizzaName: string) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId: string) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+addNewPizza({ name: "BBQ Chicken", cost: 12 })
+addNewPizza({ name: "Spicy Sausage", cost: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder("1")
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
