fa8f92cc18d6c85f7207131982a30c2c0fb28d39
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-01-22 17:41:38 +0200

    done with running the typescitpt program

--- TypeScriptLearning/index.ts
+++ TypeScriptLearning/index.ts
@@ -41,3 +41,13 @@ let something;
 something ='hello';
 something=20;
 console.log(something);
+
+let ourTuple: [number, boolean, string];
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+// We have no type safety in our tuple for indexes 3+
+ourTuple.push('Something new and wrong');
+console.log(ourTuple);
+ourTuple.push(10);
+ourTuple.push(true);
+console.log(ourTuple);
