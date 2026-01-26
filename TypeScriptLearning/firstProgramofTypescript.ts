486b3b06444d23620e84e463c96556d5d42e021f
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-01-26 10:16:13 +0200

    done with running the typescitpt program

--- TypeScriptLearning/firstProgramofTypescript.ts
+++ TypeScriptLearning/firstProgramofTypescript.ts
@@ -2,6 +2,11 @@ type Pizza = {
     name: string
     price: number
 }
+type Order ={
+    id: number
+    pizza: Pizza
+    status: "ordered"|"completed"
+}
 const menu = [
     { name: "Margherita", price: 8 },
     { name: "Pepperoni", price: 10 },
@@ -11,37 +16,41 @@ const menu = [
 
 let cashInRegister = 100
 let nextOrderId = 1
-const orderQueue=[{}]
+const orderQueue:Order[]=[];
 
 function addNewPizza(pizzaObj: Pizza) {
-    menu.push( { name: "Margherita", price: 8 })
+    menu.push( pizzaObj);
 }
 
 function placeOrder(pizzaName: string) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
     
     if(!selectedPizza){
-      console.error(`${pizzaName} does not exist in the menu`;)
+      console.error(`${pizzaName} does not exist in the menu`)
         return 
     }
     cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    const newOrder : Order = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderQueue.push(newOrder)
     return newOrder
 }
 
 function completeOrder(orderId: number) {
-    const order = orderQueue.find(order => order.id === orderId)
+    const order = orderQueue.find(order => order.id === orderId);
+    if(!order){
+        console.error(`${orderId} was not found in the orderQueue`);
+        return
+    }
     order.status = "completed"
     return order
 }
-
+ 
 addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
 addNewPizza({ name: "BBQ Chicken", price: 12 })
 addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
-completeOrder("1")
+completeOrder(1)
 
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
