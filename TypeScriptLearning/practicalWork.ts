94d8ca3b5bd58d92274ef3e4e5105c0825996e41
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-01-30 14:25:31 +0200

    done for today

--- TypeScriptLearning/practicalWork.ts
+++ TypeScriptLearning/practicalWork.ts
@@ -34,6 +34,47 @@ return `Name:${user.name}(${user.role})`
 }
 console.log(printUser(florence))
 
+//Q4 readonly
+interface Product {
+    readonly id:number
+    name:string
+    price:number
+    discount?:number
+}
+
+const florence2:Product ={
+ id:1,
+ name:"Uwineza",
+ price:1000
+}
+// console.log(florence2.id=2); it can not be 
+// changed because it is declared as the readonly
+//utility type
+//Q5
+interface Profile {
+  username: string;
+  bio: string;
+  age: number;
+}
+let myProfile:Profile={
+    username:"UWINEZA",
+    bio:"DON'T GOT ANYTHING TO SAY",
+    age:30
+}
+function beforeTheUpdate(users:Profile){
+return `before the update ${users.username} and the age is ${users.age}`
+}
+function updateProfile(users:Partial<Profile>){
+ users.username ="uwineza";
+ users.age=20;
+ return `The age of Profile is ${users.username} and the age is ${users.age}`;
+}
+console.log(beforeTheUpdate(myProfile))
+console.log(updateProfile(myProfile));
+
+//Q6
+
+
 
 
 
