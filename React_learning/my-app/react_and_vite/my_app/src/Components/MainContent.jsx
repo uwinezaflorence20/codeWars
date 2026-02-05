d4056dc3308b8918cec6295563f3d7701a730f8c
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-02-05 20:46:04 +0200

    done

---
+++ React_learning/my-app/react_and_vite/my_app/src/Components/MainContent.jsx
@@ -0,0 +1,4 @@
+function MainContent() {
+    return <h1>React is great</h1>
+}
+export default MainContent;
--- React_learning/my-app/react_and_vite/my_app/src/main.jsx
+++ React_learning/my-app/react_and_vite/my_app/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
 // import { createElement } from 'react';
 
 createRoot(document.getElementById('root')).render(
@@ -12,5 +13,14 @@ createRoot(document.getElementById('root')).render(
 const root = createRoot(document.getElementById("root"));
 const reactElement = <h1>Hello from  JSX! </h1>;
 root.render(reactElement);
+// const root2 = createRoot(document.getElementById("root"));
+// let ele = React.createElement("h1",null,"Hello, React");
+// root2.render(ele);
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is imperative coding"
+h1.className = "header"
+document.getElementById("root").appendChild(h1)
+
 
 
