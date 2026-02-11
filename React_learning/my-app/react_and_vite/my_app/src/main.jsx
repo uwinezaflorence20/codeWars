5125ea2118d8404b63d9473698d44c825bb6ec39
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-02-11 15:10:21 +0200

    done

--- React_learning/my-app/react_and_vite/my_app/src/main.jsx
+++ React_learning/my-app/react_and_vite/my_app/src/main.jsx
@@ -4,23 +4,38 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react'
 // import { createElement } from 'react';
+// import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-const root = createRoot(document.getElementById("root"));
-const reactElement = <h1>Hello from  JSX! </h1>;
-root.render(reactElement);
+// const root = createRoot(document.getElementById("root"));
+// const reactElement = <h1>Hello from  JSX! </h1>;
+// root.render(reactElement);
 // const root2 = createRoot(document.getElementById("root"));
 // let ele = React.createElement("h1",null,"Hello, React");
 // root2.render(ele);
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is imperative coding"
-h1.className = "header"
-document.getElementById("root").appendChild(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is imperative coding"
+// h1.className = "header"
+// document.getElementById("root").appendChild(h1)
 
 
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <img src="../src/assets/react.svg"/>
+<h1> Fun facts about React</h1>
+<ul>
+  <li>Was first released in 2013</li>
+  <li>Was originally created by jordan walkie</li>
+  <li>Has well over 100k hits on githubs</li>
+  <li>Is maintained by Meta</li>
+  <li>Powers thousands of rnterprise apps, including mobile apps</li>
+</ul>
 
+</div>
+)
