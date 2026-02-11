7e5c0ad20468109d9371dc3b1c92170e791f8d83
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-02-11 22:06:58 +0200

    done

--- React_learning/my-app/react_and_vite/my_app/src/App.jsx
+++ React_learning/my-app/react_and_vite/my_app/src/App.jsx
@@ -1,13 +1,17 @@
 import Header from './Components/Header'
 import Main from './Components/Main'
 import './App.css'
+import NewApp from './Components/Newapp'
+import Entry from './Components/Entry'
 function App() {
 
 
   return (
     <>
-<Header/>
-<Main/>
+ <Header/>
+<Main/> 
+<NewApp/>
+<Entry/>
           </>
   )
 }
