c294036e2291b55fd6da52234aa0bb7c1e4e0ea7
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-02-12 10:21:10 +0200

    done

--- React_learning/my-app/react_and_vite/my_app/src/App.jsx
+++ React_learning/my-app/react_and_vite/my_app/src/App.jsx
@@ -4,6 +4,21 @@ import './App.css'
 import NewApp from './Components/Newapp'
 import Entry from './Components/Entry'
 function App() {
+let firstName ="Uwineza"
+let secondName ="Florence"
+
+ const hours = new Date().getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else if (hours < 21) {
+    timeOfDay = "evening"
+  } else {
+    timeOfDay = "night"
+  } 
 
 
   return (
@@ -12,6 +27,9 @@ function App() {
 <Main/> 
 <NewApp/>
 <Entry/>
+<p>The firstname is {firstName} and the last name is {secondName}</p>
+
+<p>Good {timeOfDay}</p>
           </>
   )
 }
