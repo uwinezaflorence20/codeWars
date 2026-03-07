5883adeb3bfd30d33051f2dbd139d0624e3c3e36
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-07 22:56:24 +0200

    done with all the codewars

--- 7kyu/stalin sort.js
+++ 7kyu/stalin sort.js
@@ -2,4 +2,13 @@ function stalinSort(arr) {
   if (arr.length === 0) return;
 
   let lastSurvivor = arr[0];
+  for (let i = 1; i < arr.length; ) {
+    if (arr[i] >= lastSurvivor) {
+      lastSurvivor = arr[i];
+      i++;
+    } else {
+      arr.splice(i, 1); // remove the element
+      // console.log("Расстрелять!");
+    }
+  }
 }
