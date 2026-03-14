f9dccaf1a154324f07732d9df760139984ebf797
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-14 21:09:14 +0200

    done

--- React_learning/my-app/react_and_vite/my_app/src/Components/WindowTracker.jsx
+++ React_learning/my-app/react_and_vite/my_app/src/Components/WindowTracker.jsx
@@ -2,8 +2,6 @@ import React from "react"
 
 export default function WindowTracker() {
   const [windowWidth,setWindowWidth] = React.useState(window.innerWidth);
-
-    
     React.useEffect(() => {
         window.addEventListener("resize", function() {
            setWindowWidth(this.window.innerWidth)
