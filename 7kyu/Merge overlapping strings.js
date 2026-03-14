965fcaca15ff20dc3c4f137d4fa424d9bb628e40
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-14 21:10:44 +0200

    done

--- 7kyu/Merge overlapping strings.js
+++ 7kyu/Merge overlapping strings.js
@@ -2,5 +2,10 @@ function mergeStrings(first, second){
  let maxOverlap = Math.min(first.length, second.length);
 
   for (let i = maxOverlap; i > 0; i--) {
-  
+    if (first.slice(-i) === second.slice(0, i)) {
+      return first + second.slice(i);
+    }
+  }
+
+
 }
