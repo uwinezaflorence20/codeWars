98ab619e111464980505a223fba61ad1a7ecc4a8
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-16 18:56:15 +0200

    done

--- 7kyu/sequence to 1.js
+++ 7kyu/sequence to 1.js
@@ -1,15 +1,15 @@
-const seqToOne =(n)=>{
-let result = []
-if( n<=1){
-  for(let i = n ; i<=1;i++){
-    result.push(i);
-  }
-}
-  else{
-for(let i= n ; i>=1; i--){
-  result.push(i);
-}
-  }
-  return result;
+const seqToOne = (n) => {
+    let result = []
+    if (n <= 1) {
+        for (let i = n; i <= 1; i++) {
+            result.push(i);
+        }
+    }
+    else {
+        for (let i = n; i >= 1; i--) {
+            result.push(i);
+        }
+    }
+    return result;
 }
 console.log(seqToOne(100));
