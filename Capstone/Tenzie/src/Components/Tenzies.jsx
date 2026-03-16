83a2c197b695a03a03338e3fe2b574bf142779cb
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-16 18:52:39 +0200

    done with little design

---
+++ Capstone/Tenzie/src/Components/Tenzies.jsx
@@ -0,0 +1,7 @@
+export default function Tenzies() {
+  return (
+    <div className=" h-screen py-10 pt-20 px-10  bg-gray-900">
+      <div className=" flex justify-center items-center h-11/12 rounded-2xl pt-20 bg-white"></div>
+    </div>
+  );
+}
