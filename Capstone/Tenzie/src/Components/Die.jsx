5bee3a6da2bc29b76f2708c44b87634b6d8b484d
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-16 19:39:06 +0200

    done

---
+++ Capstone/Tenzie/src/Components/Die.jsx
@@ -0,0 +1,5 @@
+export default function Die(props){
+    return(
+        <button className="w-10 bg-amber-100 rounded-4xl ">{props.value}</button>
+    )
+}
--- Capstone/Tenzie/src/Components/Tenzies.jsx
+++ Capstone/Tenzie/src/Components/Tenzies.jsx
@@ -1,7 +1,19 @@
+import Die from "./Die";
 export default function Tenzies() {
   return (
     <div className=" h-screen py-10 pt-20 px-10  bg-gray-900">
-      <div className=" flex justify-center items-center h-11/12 rounded-2xl pt-20 bg-white"></div>
+      <div className="  flex justify-center items-center gap-4 h-11/12 rounded-2xl pt-20 bg-white">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
     </div>
   );
 }
