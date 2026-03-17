895ec1236dad040b47c6a7125a05bac107645f38
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-17 16:46:39 +0200

    done

--- 6kyu/string transformation.js
+++ 6kyu/string transformation.js
@@ -1,5 +1,17 @@
 function stringTransformation(str,ops){
     for(let op of ops){
-        
+        if (op === "U") {
+            str = str.toUpperCase();
+        } else if (op === "L") {
+            str = str.toLowerCase();
+        } else if (op === "R") {
+            str = str.split("").reverse().join("");
+        } else if (op === "D") {
+            let result = "";
+            for (let ch of str) {
+                result += ch + ch;
+            }
+            str = result;
+        }
     }
 }
