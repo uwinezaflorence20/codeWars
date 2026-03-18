b96f4a48b99b64bb26b874fe2f5f549a096a5130
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-18 14:20:44 +0200

    done

--- 6kyu/speech to text.js
+++ 6kyu/speech to text.js
@@ -8,6 +8,10 @@ function siri(input) {
     const nums = input.match(/\d+/g).map(Number);
     console.log(nums);
     if (input.includes("Add")) return nums[0] + nums[1];
-   
+    if (input.includes("Substract")) return nums[0] - nums[1];
+    if (input.includes("weather")) {
+        let time = input.match(/\d+/g)
+        console.lo
+    }
 }
 console.log(bot("Add 5 to 20."))
