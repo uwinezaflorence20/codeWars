9cb1ec346a35ab3a650ee768d21e059e7d26139e
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-18 15:34:38 +0200

    done well

--- Capstone/Router/src/App.jsx
+++ Capstone/Router/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Link, BrowserRouter } from "react-router-dom";
 import Basic from "./Components/Basic";
 import About from "./Components/About";
 function App() {
   return (
+    <BrowserRouter>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
     <Routes>
       <Route path="/" element={<Basic/>}/>
       <Route path="/about" element={<About/>}/>
     </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
