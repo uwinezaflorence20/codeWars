9e6343cd89ef2482b38dcaa01b7844f980054151
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-18 14:20:16 +0200

    done with the second push

--- 6kyu/speech to text.js
+++ 6kyu/speech to text.js
@@ -5,6 +5,9 @@ const bot = {
 };
 
 function siri(input) {
-
+    const nums = input.match(/\d+/g).map(Number);
+    console.log(nums);
+    if (input.includes("Add")) return nums[0] + nums[1];
+   
 }
 console.log(bot("Add 5 to 20."))
