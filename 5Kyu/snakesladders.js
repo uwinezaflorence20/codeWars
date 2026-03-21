3bb9bf96647ec224a02e82c765e995ecb1c6a4f5
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-21 21:57:36 +0200

    done

--- 5Kyu/snakesladders.js
+++ 5Kyu/snakesladders.js
@@ -4,3 +4,12 @@ class SnakesLadders {
     this.player2 = 0;
     this.currentPlayer = 1;
     this.gameOver = false;
+    this.board = {
+      2: 38, 7: 14, 8: 31, 15: 26, 21: 42,
+      28: 84, 36: 44, 51: 67, 71: 91, 78: 98,
+      87: 94,
+      16: 6, 46: 25, 49: 11, 62: 19,
+      64: 60, 74: 53, 89: 68, 92: 88,
+      95: 75, 99: 80
+    };
+  }
