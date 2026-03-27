eb7ca7df81c765e8b0203741192a4eb613dfd261
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-27 15:52:22 +0200

    done well with the sidebar

--- Capstone/Router/src/Layout.jsx
+++ Capstone/Router/src/Layout.jsx
@@ -4,8 +4,7 @@ const Layout =()=>{
         <div className="h-56 w-[100%] flex">
             <div className="bg-blue-300 w-[30%] flex flex-col">
                <Link to="dashboard" className="p-4  font-bold text-xl rounded-y-2xl">Dashboard</Link>
-               <Link to="dashboard" className="p-4  font-bold text-xl rounded-y-2xl">report</Link>
-               <Link to="dashboard" className="p-4  font-bold text-xl rounded-y-2xl">image</Link>
+               <Link to="dashboard" className="p-4  font-bold text-xl rounded-y-2xl">Users</Link>
                <Link to="dashboard" className="p-4  font-bold text-xl rounded-y-2xl">setting</Link>
             </div>
             <div className="bg-red-500 w-[70%]">
