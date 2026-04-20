862198eac06564e7472db5d23ef683b497176847
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-20 18:16:25 +0200

    done well with the api context

---
+++ Redux/redux_app/src/components/Example3.js
@@ -0,0 +1,35 @@
+import React,{useState,createContext,useContext} from 'react'
+const AppContext = createContext(null);
+export const Example3=()=> {
+const[userName,setUserName] = useState("PedroTech");
+const[count,setCount] = useState(0);
+  return (
+    <AppContext.Provider value={{userName,setUserName,count,setCount}}>
+        <Child1/>
+        <Child2/>
+        <Child3/>
+        <Child4/>
+    </AppContext.Provider>
+  )
+}
+
+export const Child1 = () =>{
+    const {userName} = useContext(AppContext);
+    return <h1>{userName}</h1>;
+};
+
+export const Child3 = () =>{
+   const {setCount} = useContext(AppContext);
+   return(
+    <div>
+        <button
+        onClick={()=>{
+            setCount((c)=>c+1);
+        }}
+        >
+change Count
+        </button>
+    </div>
+   )
+};
+
