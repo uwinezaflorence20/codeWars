7aacbf03e33277b43d0592d30ccf887defbb3ba4
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-20 16:04:17 +0200

    done with the first way using the XOR

--- 6kyu/Find the unique number.js
+++ 6kyu/Find the unique number.js
@@ -8,3 +8,13 @@ function findUnique(arr) {
 console.log(findUnique( [1, 8, 4, 4, 6, 1, 8]))
 
 // second way
+let findUnique =(arr)=> {
+if(arr.length === 0) return [];
+let sorted = arr.sort((a,b)=> a-b);
+let result =[];
+for(let i =0 ; i<=sorted.length;i+=2){
+    if(sorted[i] !== sorted[i+1]){
+       return sorted[i];
+    }
+}
+}
