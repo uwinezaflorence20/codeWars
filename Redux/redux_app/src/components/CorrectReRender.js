15d221e17c6e09646336cd93027b9c489100c995
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-21 18:29:41 +0200

    done well

--- Redux/redux_app/src/components/CorrectReRender.js
+++ Redux/redux_app/src/components/CorrectReRender.js
@@ -1,7 +1,7 @@
 import React,{createContext,useState} from 'react'
 export const UserContext = createContext(null);
 
-export const CorrectReRender=()=> {
+export const CorrectReRender=({childeren})=> {
     const [user,setUser] = useState(null);
     const [isAuth,setIsAuth] = useState(false);
     const [isPremierUser,setIsPremierUser] = useState(false);
@@ -14,9 +14,7 @@ export const CorrectReRender=()=> {
         setIsPremierUser
     }
   return (
-    <div>
-      
-    </div>
+   <UserContext.Provider value={value}> {childeren}</UserContext.Provider>
   )
 }
 
