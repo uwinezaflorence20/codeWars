07ebd4e4cc89ed175312c6e33a42c55cb19ea4c1
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-09-11 12:33:10 +0200

    done with creating the users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param,Patch,Post, Query } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users')// /users
+export class UsersController {
+    /*
+    GET /users
+    GET /users/:id
+    POST /users/
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+   @Get() // GET /users or /users?role=value
+   findAll(@Query('role') role?:"INTERN" | "ENGINEER" | "ADMIN"){
+    return []
+   }
+//    @Get('interns') // GET / users/interns
+//    findAllInterns(){
+//     return []
+//    }
+
+   @Get(':id') //  GET /users/:id
+   findOne(@Param('id') id: string){
+    return {id}
+   }
+   @Post() // posting or creating the user
+   create(@Body() user:{}){
+      return user
+   }
+   @Patch(':id') //  GET /users/:id
+   update(@Param('id') id: string, @Body() userUpdate: {}){
+    return {id, ...userUpdate}
+   }
+
+   @Delete(':id') //  GET /users/:id
+   delete(@Param('id') id: string){
+    return {id}
+   }
+
+   
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
 
 @Module({
     controllers: [UsersController],
-    providers: [ ]
+    providers: [ UsersService]
 })
 export class UsersModule {}
+
